f2708f4b37c4f7410ecbe76be5d2e0f2ccd24b68
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-03-23 12:09:01 +0600

    + nuget

--- src/Utils/nuget-publish.ps1
+++ src/Utils/nuget-publish.ps1
@@ -1,5 +1,5 @@
 param (    
     [Parameter(Mandatory=$true)][string]$apiKey
 )
-dotnet nuget push MG.Utils.1.0.0.nupkg --api-key $apiKey --source https://api.nuget.org/v3/index.json
+dotnet nuget push MaximGorbatyuk.Utils.1.0.0.nupkg --api-key $apiKey --source https://api.nuget.org/v3/index.json
 
