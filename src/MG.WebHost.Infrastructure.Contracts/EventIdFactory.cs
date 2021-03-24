e38237cfc135cddc45c0df5b3070d0e9e798f8e5
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-03-24 15:17:17 +0600

    + fix of folder related error

--- src/MG.WebHost.Infrastructure.Contracts/EventIdFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace MG.WebHost.Infrastructure.Contracts
-{
-    public static class EventIdFactory
-    {
-        public static EventId ConsumerError => new EventId(1234);
-    }
-}
