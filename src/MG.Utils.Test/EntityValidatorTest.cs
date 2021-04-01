ff53b1d2d3b89e15659f3568ed3f88a8618c00b8
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-04-01 14:38:12 +0600

    Have added tests

--- src/MG.Utils.Test/EntityValidatorTest.cs
+++ src/MG.Utils.Test/EntityValidatorTest.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using MG.Utils.Exceptions;
-using MG.Utils.I18N;
 using MG.Utils.Validators;
 using Xunit;
 
@@ -49,17 +48,53 @@ namespace MG.Utils.Test
             Assert.Throws<EntityInvalidException>(() => target.ThrowIfInvalid());
         }
 
+        [Fact]
+        public void ThrowIfInvalid_ChildClass_NoErrors_Ok()
+        {
+            var target = new EntityValidator<EntityExtended>(new EntityExtended
+            {
+                RequiredTitle = "Awesome",
+                NonRequiredTitle = null,
+                Year = 2020,
+                AdditionalRequiredTitle = "Hello"
+            });
+
+            Assert.True(target.Valid());
+
+            target.ThrowIfInvalid();
+        }
+
+        [Fact]
+        public void ThrowIfInvalid_ChildClass_ParentHasErrors_Exception()
+        {
+            var target = new EntityValidator<EntityExtended>(new EntityExtended
+            {
+                AdditionalRequiredTitle = "Hello"
+            });
+
+            Assert.False(target.Valid());
+
+            Assert.Throws<EntityInvalidException>(() => target.ThrowIfInvalid());
+        }
+
         public class Entity
         {
-            [Required(ErrorMessage = DataAnnotationErrorMessages.Required)]
-            [StringLength(10, ErrorMessage = DataAnnotationErrorMessages.StringLength)]
-            public string RequiredTitle { get; set; }
+            [Required]
+            [StringLength(10)]
+            public string RequiredTitle { get; init; }
 
-            [StringLength(15, ErrorMessage = DataAnnotationErrorMessages.StringLength)]
-            public string NonRequiredTitle { get; set; }
+            [StringLength(15)]
+            public string NonRequiredTitle { get; init; }
 
             [Range(1900, 3000)]
-            public int Year { get; set; }
+            public int Year { get; init; }
+        }
+
+        public class EntityExtended : Entity
+        {
+            [Required]
+            [StringLength(10)]
+            public string AdditionalRequiredTitle { get; init; }
         }
     }
 }
