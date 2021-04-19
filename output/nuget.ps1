b2ad30698d45e604acafe0810cbf600d2b55b767
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-04-19 12:46:35 +0600

    Have fixed a script

--- output/nuget.ps1
+++ output/nuget.ps1
@@ -1,32 +1,44 @@
 #Get-ChildItem -Path ./MG*/*.nuspec
 
 
-dotnet build ./../MG.Utils/MG.Utils.csproj
-nuget pack ./../MG.Utils/MG.Utils.nuspec
+dotnet build ./../src/MG.Utils/
+nuget pack ./../src/MG.Utils/
 
-dotnet build ./../MG.Utils/MG.Utils.Abstract.csproj
-nuget pack ./../MG.Utils.Abstract/MG.Utils.Abstract.nuspec
+dotnet build ./../src/MG.Utils.Abstract/
+nuget pack ./../src/MG.Utils.Abstract/
 
-nuget pack ./../MG.Utils.AspNetCore/MG.Utils.AspNetCore.nuspec
+dotnet build ./../src/MG.Utils.AspNetCore/
+nuget pack ./../src/MG.Utils.AspNetCore/
 
-nuget pack ./../MG.Utils.AspNetCore.I18N/MG.Utils.AspNetCore.I18N.nuspec
+dotnet build ./../src/MG.Utils.AspNetCore.I18N/
+nuget pack ./../src/MG.Utils.AspNetCore.I18N/
 
-nuget pack ./../MG.Utils.AspNetCore.Middlewares/MG.Utils.AspNetCore.Middlewares.nuspec
+dotnet build ./../src/MG.Utils.AspNetCore.Middlewares/
+nuget pack ./../src/MG.Utils.AspNetCore.Middlewares/
 
-nuget pack ./../MG.Utils.AspNetCore.Redis/MG.Utils.AspNetCore.Redis.nuspec
+dotnet build ./../src/MG.Utils.AspNetCore.Redis/
+nuget pack ./../src/MG.Utils.AspNetCore.Redis/
 
-nuget pack ./../MG.Utils.AspNetCore.Swagger/MG.Utils.AspNetCore.Swagger.nuspec
+dotnet build ./../src/MG.Utils.AspNetCore.Swagger/
+nuget pack ./../src/MG.Utils.AspNetCore.Swagger/
 
-nuget pack ./../MG.Utils.Authentication/MG.Utils.Authentication.nuspec
+dotnet build ./../src/MG.Utils.Authentication/
+nuget pack ./../src/MG.Utils.Authentication/
 
-nuget pack ./../MG.Utils.Azure.Authentication/MG.Utils.Azure.Authentication.nuspec
+dotnet build ./../src/MG.Utils.Azure.Authentication/
+nuget pack ./../src/MG.Utils.Azure.Authentication/
 
-nuget pack ./../MG.Utils.Azure.Logging/MG.Utils.Azure.Logging.nuspec
+dotnet build ./../src/MG.Utils.Azure.Logging/
+nuget pack ./../src/MG.Utils.Azure.Logging/
 
-nuget pack ./../MG.Utils.Azure.ServiceBus/MG.Utils.Azure.ServiceBus.nuspec
+dotnet build ./../src/MG.Utils.Azure.ServiceBus/
+nuget pack ./../src/MG.Utils.Azure.ServiceBus/
 
-nuget pack ./../MG.Utils.Export/MG.Utils.Export.nuspec
+dotnet build ./../src/MG.Utils.Export/
+nuget pack ./../src/MG.Utils.Export/
 
-nuget pack ./../MG.Utils.Smtp/MG.Utils.Smtp.nuspec
+dotnet build ./../src/MG.Utils.Smtp/
+nuget pack ./../src/MG.Utils.Smtp/
 
-nuget pack ./../MG.Utils.Validation/MG.Utils.Validation.nuspec
+dotnet build ./../src/MG.Utils.Validation/
+nuget pack ./../src/MG.Utils.Validation/
