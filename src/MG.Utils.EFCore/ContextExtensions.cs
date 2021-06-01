721b84bee6009ab06b25685f0eae4e656ea50721
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-06-01 09:42:39 +0600

    + cancellation token

--- src/MG.Utils.EFCore/ContextExtensions.cs
+++ src/MG.Utils.EFCore/ContextExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 using MG.Utils.Abstract;
 using MG.Utils.Abstract.Entities;
@@ -22,20 +23,20 @@ namespace MG.Utils.EFCore
             return set.Where(x => x.DeletedAt == null);
         }
 
-        public static async Task<TEntity> AddEntityAsync<TContext, TEntity>(this TContext set, TEntity entity)
+        public static async Task<TEntity> AddEntityAsync<TContext, TEntity>(this TContext set, TEntity entity, CancellationToken cancellationToken = default(CancellationToken))
             where TContext : DbContext
             where TEntity : class
         {
-            var entry = await set.AddAsync(entity);
+            var entry = await set.AddAsync(entity, cancellationToken);
 
             return entry.Entity;
         }
 
-        public static async Task AddRangeAsync<TContext, TEntity>(this TContext set, IEnumerable<TEntity> entity)
+        public static async Task AddRangeAsync<TContext, TEntity>(this TContext set, IEnumerable<TEntity> entity, CancellationToken cancellationToken = default(CancellationToken))
             where TContext : DbContext
             where TEntity : class
         {
-            await set.AddRangeAsync(entity);
+            await set.AddRangeAsync(entity, cancellationToken);
         }
 
         /// <summary>
@@ -43,68 +44,71 @@ namespace MG.Utils.EFCore
         /// </summary>
         /// <typeparam name="T">Type.</typeparam>
         /// <param name="query">Query.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         /// <returns>Array.</returns>
-        public static Task<T[]> AllAsync<T>(this IQueryable<T> query)
+        public static Task<T[]> AllAsync<T>(this IQueryable<T> query, CancellationToken cancellationToken = default(CancellationToken))
             where T : class
         {
             return query
                 .AsNoTracking()
-                .ToArrayAsync();
+                .ToArrayAsync(cancellationToken);
         }
 
-        public static async Task<TResultEntity[]> AllAsync<TEntity, TResultEntity>(this IQueryable<TEntity> query, Func<TEntity, TResultEntity> transform)
+        public static async Task<TResultEntity[]> AllAsync<TEntity, TResultEntity>(
+            this IQueryable<TEntity> query, Func<TEntity, TResultEntity> transform, CancellationToken cancellationToken = default(CancellationToken))
             where TEntity : class
             where TResultEntity : class
         {
             return (await query
-                .AllAsync())
+                .AllAsync(cancellationToken))
                 .Select(transform)
                 .ToArray();
         }
 
-        public static Task<T> ByIdOrNullAsync<T>(this IQueryable<T> query, long id)
+        public static Task<T> ByIdOrNullAsync<T>(this IQueryable<T> query, long id, CancellationToken cancellationToken = default(CancellationToken))
             where T : class, IHasId
         {
-            return query.FirstOrDefaultAsync(x => x.Id == id);
+            return query.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
         }
 
-        public static async Task HasEntityOrFailAsync<T>(this IQueryable<T> query, long id)
+        public static async Task HasEntityOrFailAsync<T>(this IQueryable<T> query, long id, CancellationToken cancellationToken = default(CancellationToken))
             where T : class, IHasId
         {
-            if (!await query.AnyAsync(x => x.Id == id))
+            if (!await query.AnyAsync(x => x.Id == id, cancellationToken))
             {
                 throw ResourceNotFoundException.CreateFromEntity<T>(id);
             }
         }
 
-        public static async Task HasEntitiesOrFailAsync<T>(this IQueryable<T> query, IReadOnlyCollection<long> ids)
+        public static async Task HasEntitiesOrFailAsync<T>(this IQueryable<T> query, IReadOnlyCollection<long> ids, CancellationToken cancellationToken = default(CancellationToken))
             where T : class, IHasId
         {
-            if (ids.Except(await query.Select(x => x.Id).ToArrayAsync()).Any())
+            if (ids.Except(await query.Select(x => x.Id).ToArrayAsync(cancellationToken)).Any())
             {
                 throw new BadRequestException($"Incorrect {typeof(T).Name} ids");
             }
         }
 
         public static async Task<IReadOnlyCollection<TDto>> MapAsync<TEntity, TDto>(
-            this IQueryable<TEntity> query, Func<TEntity, TDto> converter)
+            this IQueryable<TEntity> query, Func<TEntity, TDto> converter, CancellationToken cancellationToken = default(CancellationToken))
             where TEntity : class
         {
-            return (await query.AllAsync())
+            return (await query.AllAsync(cancellationToken))
                 .Select(converter)
                 .ToArray();
         }
 
-        public static async Task<T> ByIdOrFailAsync<T>(this IQueryable<T> query, long id)
+        public static async Task<T> ByIdOrFailAsync<T>(this IQueryable<T> query, long id, CancellationToken cancellationToken = default(CancellationToken))
             where T : class, IHasId
         {
-            return await query.FirstOrDefaultAsync(x => x.Id == id)
+            return await query.FirstOrDefaultAsync(x => x.Id == id, cancellationToken)
                    ?? throw ResourceNotFoundException.CreateFromEntity<T>(id);
         }
 
         public static async Task<Pageable<TEntity>> AsPaginatedAsync<TEntity>(
             this IQueryable<TEntity> query,
-            PageModel pageModelOrNull = null)
+            PageModel pageModelOrNull = null,
+            CancellationToken cancellationToken = default(CancellationToken))
             where TEntity : class
         {
             pageModelOrNull ??= PageModel.Default;
@@ -112,17 +116,18 @@ namespace MG.Utils.EFCore
             return new Pageable<TEntity>(
                 currentPage: pageModelOrNull.Page,
                 pageSize: pageModelOrNull.PageSize,
-                totalItems: await query.CountAsync(),
+                totalItems: await query.CountAsync(cancellationToken),
                 results: await query
                     .Skip(pageModelOrNull.ToSkip)
                     .Take(pageModelOrNull.PageSize)
-                    .AllAsync());
+                    .AllAsync(cancellationToken));
         }
 
         public static async Task<Pageable<TResultEntity>> AsPaginatedAsync<TEntity, TResultEntity>(
             this IQueryable<TEntity> query,
             Func<TEntity, TResultEntity> transform,
-            PageModel pageModelOrNull = null)
+            PageModel pageModelOrNull = null,
+            CancellationToken cancellationToken = default(CancellationToken))
             where TEntity : class
             where TResultEntity : class
         {
@@ -131,12 +136,12 @@ namespace MG.Utils.EFCore
             return new Pageable<TResultEntity>(
                 currentPage: pageModelOrNull.Page,
                 pageSize: pageModelOrNull.PageSize,
-                totalItems: await query.CountAsync(),
+                totalItems: await query.CountAsync(cancellationToken),
                 results: (await query
                     .Skip(pageModelOrNull.ToSkip)
                     .Take(pageModelOrNull.PageSize)
                     .AsNoTracking()
-                    .ToArrayAsync())
+                    .ToArrayAsync(cancellationToken))
                 .Select(transform)
                 .ToArray());
         }
@@ -153,10 +158,11 @@ namespace MG.Utils.EFCore
         public static async Task AnyOrFailAsync<T>(
             this IQueryable<T> context,
             Expression<Func<T, bool>> expression,
-            string errorMessage = null)
+            string errorMessage = null,
+            CancellationToken cancellationToken = default(CancellationToken))
             where T : class
         {
-            if (!await context.AnyAsync(expression))
+            if (!await context.AnyAsync(expression, cancellationToken))
             {
                 throw new ResourceNotFoundException(errorMessage ?? "There is no item found by a passed condition");
             }
@@ -165,10 +171,11 @@ namespace MG.Utils.EFCore
         public static async Task NoItemsByConditionOrFailAsync<T>(
             this IQueryable<T> context,
             Expression<Func<T, bool>> expression,
-            string errorMessage = null)
+            string errorMessage = null,
+            CancellationToken cancellationToken = default(CancellationToken))
             where T : class
         {
-            if (await context.AnyAsync(expression))
+            if (await context.AnyAsync(expression, cancellationToken))
             {
                 throw new BadRequestException(errorMessage ?? "There are items found by a passed condition");
             }
@@ -177,21 +184,22 @@ namespace MG.Utils.EFCore
         public static async Task NoItemsByConditionOrFailAsync<T>(
             this IQueryable<T> context,
             Expression<Func<T, bool>> expression,
-            Exception exception)
+            Exception exception,
+            CancellationToken cancellationToken = default(CancellationToken))
             where T : class
         {
-            if (await context.AnyAsync(expression))
+            if (await context.AnyAsync(expression, cancellationToken))
             {
                 throw exception;
             }
         }
 
-        public static async Task<int> TrySaveChangesAsync<TContext>(this TContext context)
+        public static async Task<int> TrySaveChangesAsync<TContext>(this TContext context, CancellationToken cancellationToken = default(CancellationToken))
             where TContext : DbContext
         {
             try
             {
-                return await context.SaveChangesAsync();
+                return await context.SaveChangesAsync(cancellationToken);
             }
             catch (Exception exception)
             {
@@ -227,24 +235,25 @@ namespace MG.Utils.EFCore
         public static async Task<TResult> DoWithinTransactionAsync<TContext, TResult>(
             this TContext context,
             Func<Task<TResult>> action,
-            string errorMessage = null)
+            string errorMessage = null,
+            CancellationToken cancellationToken = default(CancellationToken))
             where TContext : DbContext
         {
             action.ThrowIfNull(nameof(action));
 
             try
             {
-                await context.Database.BeginTransactionAsync();
+                await context.Database.BeginTransactionAsync(cancellationToken);
 
                 TResult result = await action();
 
-                await context.Database.CommitTransactionAsync();
+                await context.Database.CommitTransactionAsync(cancellationToken);
 
                 return result;
             }
             catch (Exception exception)
             {
-                await context.Database.RollbackTransactionAsync();
+                await context.Database.RollbackTransactionAsync(cancellationToken);
                 const string defaultError = "Cannot execute transaction due to database error";
                 throw new InvalidOperationException(errorMessage ?? defaultError, exception);
             }
