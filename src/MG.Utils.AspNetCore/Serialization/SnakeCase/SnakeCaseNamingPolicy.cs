6fc9b801480d21eb9d1b012e7822a426bedf1b9a
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2021-06-18 09:30:39 +0600

    Have removed newtonsoft as a dependency

--- src/MG.Utils.AspNetCore/Serialization/SnakeCase/SnakeCaseNamingPolicy.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json;
-
-namespace MG.Utils.AspNetCore.Serialization.SnakeCase
-{
-    public class SnakeCaseNamingPolicy : JsonNamingPolicy
-    {
-        public override string ConvertName(string name) => name.ToSnakeCase();
-    }
-}
