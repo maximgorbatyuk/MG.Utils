30997b470dea99f81f92d9b04c7753a941177367
Madi <m.abdigaliyev@petrel.ai>
2021-11-18 23:06:42 +0600

    changing test AsByteArray_ReturnsByteArrayAsync

--- src/MG.Utils.Export.Test/HtmlToPdfTests.cs
+++ src/MG.Utils.Export.Test/HtmlToPdfTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Text;
+using System.Threading.Tasks;
 using DinkToPdf.Contracts;
 using MG.Utils.Export.PdfServices;
 using Moq;
@@ -11,12 +12,14 @@ namespace MG.Utils.Export.Test
         [Fact]
         public async Task AsByteArray_ReturnsByteArrayAsync()
         {
+            string testingText = "<p>Html Content</p>";
             var converterMoq = new Mock<IConverter>();
             converterMoq.Setup(x => x.Convert(It.IsAny<IDocument>()))
-                .Returns(It.IsAny<byte[]>);
+                .Returns(Encoding.ASCII.GetBytes(testingText));
 
-            var htmlToPdf = new HtmlToPdf("<p>Html Content</p>", converterMoq.Object);
+            var htmlToPdf = new HtmlToPdf(testingText, converterMoq.Object);
             var result = await htmlToPdf.AsByteArrayAsync();
+            Assert.Equal(testingText, Encoding.ASCII.GetString(result));
         }
     }
 }
